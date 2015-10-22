af97c351c9e5bda1a69e88b0f813518ed07ba8be
sandhage <kylesandhage@gmail.com>
2015-10-22 13:37:31 -0400

    Made basic CSS layout, more work on app.js

--- js/app.js
+++ js/app.js
@@ -1,15 +1,20 @@
 // Variables
 var fizzCount = 0;
-var node = document.getElementById('fizz-list')
+var node = document.getElementById('fizzlist');
+
 // Document-Ready Functions
 $(document).ready(function() {
 	
-	for ( fizzCount < 100; fizzCount++ ) {
-		if ( fizzCount % 3 == 0 ) {
-			node.innerHTML('<li>Fizz!</li>')
-		} else if ( fizzCount % 5 == 0 ) {
-			node.innerHTML('<li>Buzz!</li>')
-		}
-	}
+	for ( fizzCount = 0; fizzCount < 100; fizzCount++ ) {
+		if ( fizzCount % 3 == 0 && fizzCount % 5 !== 0 ) {
+			node.innerHTML( '<li>Fizz!</li>' );
+		} else if ( fizzCount % 3 !== 0 && fizzCount % 5 == 0 ) {
+			node.innerHTML( '<li>Buzz!</li>' );
+		} else if ( fizzCount % 3 == 0 && fizzCount % 5 == 0) {
+			node.innerHTML( '<li>FizzBuzz!</li>' );
+		} else if ( fizzCount % 3 !== 0 && fizzCount % 5 !== 0) {
+			node.innerHTML( '<li>' + fizzCount + '</li>' );
+		};
+	};
 
 });
