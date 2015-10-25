00cdb6f372a2f4362a682c9c2c68e324ed0ce647
sandhage <kylesandhage@gmail.com>
2015-10-25 14:45:59 -0400

    Refactor: COMPLETE

--- js/app.js
+++ js/app.js
@@ -1,21 +1,24 @@
 // Document-Ready Functions
 document.addEventListener("DOMContentLoaded", function(event) {
 	
+	var fizzPrompt = prompt("How far do you want to take this thing?", "Enter");
+	fizzPrompt = parseInt(fizzPrompt, 10);
+
 	var node = document.getElementById('fizzlist');
 
-	for ( var fizzCount = 1; fizzCount <= 100; fizzCount++ ) {
+	for ( var fizzCount = 1; fizzCount <= fizzPrompt; fizzCount++ ) {
 		if ( fizzCount % 3 == 0 && fizzCount % 5 == 0) {
 			node.innerHTML = node.innerHTML + '<li>FizzBuzz!</li>';
-			console.log('fizzbuzz');
+			// console.log('fizzbuzz');
 		} else if ( fizzCount % 3 == 0 ) {
 			node.innerHTML = node.innerHTML + '<li>Fizz!</li>';
-			console.log('fizz');
+			// console.log('fizz');
 		} else if ( fizzCount % 5 == 0 ) {
 			node.innerHTML = node.innerHTML + '<li>Buzz!</li>';
-			console.log('buzz');
+			// console.log('buzz');
 		} else {
 			node.innerHTML = node.innerHTML + '<li>' + fizzCount + '</li>';
-			console.log(fizzCount);
+			// console.log(fizzCount);
 		};
 	};
 
